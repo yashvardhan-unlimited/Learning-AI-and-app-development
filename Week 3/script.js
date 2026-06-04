1aeb8ec68f6505e15a06ba4d703c630b71347c4a
Yashvardhan Gupta <dev.yash.g8@gmail.com>
2026-06-04 23:36:04 +0530

    updating week 3 files

--- Week 3/script.js
+++ Week 3/script.js
@@ -1,2 +1,11 @@
-// I will be adding my JavaScript code here
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
 
