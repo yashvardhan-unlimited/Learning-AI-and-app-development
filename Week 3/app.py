49b36e0e3b379d3d3182685af4cbc6c3c7e41280
Yashvardhan Gupta <dev.yash.g8@gmail.com>
2026-06-13 23:18:59 +0530

    updates..

--- Week 3/app.py
+++ Week 3/app.py
@@ -8,9 +8,56 @@ def banklogin():
     # Get the username and password from the form.
     username = request.form.get("username")
     password = request.form.get("password")
-
     # Find the user in the database by username and password.
     # In a real application, you should never store passwords in plain text.
     # Instead, you should hash the password using a secure hashing algorithm like bcrypt,
     # and then store the hashed password in the database. When verifying the password,
-    # Anew feature
+    # Anew feature 2 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# A new line of code
